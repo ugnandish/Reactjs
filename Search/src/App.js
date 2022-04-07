2d370c1851ae52d07b3fa8b54ed316b6beb126ba
ugnandish <nandish.ug@gmail.com>
2022-04-07 12:22:30 +0530

    Add files via upload

---
+++ Search/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './components/Card';
+import Nav from './components/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
